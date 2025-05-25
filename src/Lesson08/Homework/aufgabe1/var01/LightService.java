1deaf97742593ed3f1e178a3c544a966e43d17e3
Dmytro Mietolkin <aldima121964@gmail.com>
2025-05-25 17:34:53 +0200

    добавление

---
+++ src/Lesson08/Homework/aufgabe1/var01/LightService.java
@@ -0,0 +1,31 @@
+package Lesson08.Homework.aufgabe1.var01;
+
+import java.util.Scanner;
+
+public class LightService {
+
+    public void lightStatus(Light light) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Включить свет? (true/false): ");
+        boolean newStatus = scanner.nextBoolean();
+        light.setOn(newStatus);
+        System.out.println("Новый статус света: " + newStatus);
+
+    }
+
+    public void ChangeBrightness(Light light){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите новую яркость (0–100):");
+        int newBrightness = scanner.nextInt();
+
+        light.setBrightness(newBrightness);
+
+        System.out.println( " Новая яркость  : " + light.getBrightness());
+
+
+    }
+    public void printStatus(Light light){System.out.println(light);}
+
+
+
+}
