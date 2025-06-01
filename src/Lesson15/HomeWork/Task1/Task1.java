f72bf68d86824713365e5ed48b1f8bf0aecd01ea
Dmytro Mietolkin <aldima121964@gmail.com>
2025-06-01 18:57:16 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/Lesson15/HomeWork/Task1/Task1.java
@@ -0,0 +1,32 @@
+package Lesson15.HomeWork.Task1;
+
+import java.util.Random;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] numbers = new int[8];
+
+        // Заполнение массива случайными числами от 1 до 50
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(50) + 1; // [0,49] + 1 = [1,50]
+        }
+
+        // Вывод исходного массива
+        System.out.print("Исходный массив: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        // Замена элементов с нечетным индексом на 0
+        for (int i = 1; i < numbers.length; i += 2) {
+            numbers[i] = 0;
+        }
+
+        // Вывод изменённого массива
+        System.out.print("\nИзменённый массив: ");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
