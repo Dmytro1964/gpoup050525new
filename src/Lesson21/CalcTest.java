34f3e218adee52c3a16b45abcd0bc8b34fd665bf
Dmytro Mietolkin <aldima121964@gmail.com>
2025-06-12 07:49:18 +0200

    homework

---
+++ src/Lesson21/CalcTest.java
@@ -0,0 +1,21 @@
+package Lesson21;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+
+
+    @Test
+    void testSum(){
+
+        Calc calc = new Calc();
+
+        int expected = 25;
+        int actual = calc.sum(10,15);
+
+        assertEquals(expected,actual);
+    }
+
+}
